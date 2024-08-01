87d1530d4796e7da909c3f24e527660cecfe59a3
Istvan Acs <istvan.acs89@gmail.com>
2024-08-01 11:00:18 +0100

    Create a floating navigation

---
+++ components/ui/floating-navbar/floating-navbar.type.ts
@@ -0,0 +1,8 @@
+export type FloatingNavbarProps = {
+  navItems: {
+    name: string;
+    link: string;
+    icon?: JSX.Element;
+  }[];
+  className?: string;
+};
---
+++ components/ui/index.ts
@@ -0,0 +1,4 @@
+export { TextGenerateEffect } from "./text-generate-effect";
+export { Spotlight } from "./spotlight";
+export { MagicButton } from "./magic-button";
+export { FloatingNav } from "./floating-navbar";
